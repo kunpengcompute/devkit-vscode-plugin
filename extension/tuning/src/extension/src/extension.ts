c4cdef89e206d9d0b38af6f643ec5c00d3dad716
Russell Han <49119975+RossHann@users.noreply.github.com>
2022-11-23 17:11:56 +0800

    feat: ip column done

--- extension/tuning/src/extension/src/extension.ts
+++ extension/tuning/src/extension/src/extension.ts
@@ -1,18 +1,25 @@
 import * as vscode from 'vscode';
-import { Utils } from './utils';
-import { ToolPanelManager } from './panel-manager';
+import {Utils} from './utils';
+import {ToolPanelManager} from './panel-manager';
 import * as sidebar from './TreeViewProvider';
+import {SideViewProvider} from './SideView/SideViewProvider';
 
 export function activate(context: vscode.ExtensionContext) {
-   // 响应VSCode配置修改
-   Utils.addConfigListening();
-   // 清除Vscode缓存信息
-   Utils.initVscodeCache(context, true);
-   // 响应perfadvisor左侧菜单树所有按钮的命令来打开不同的webview
-   ToolPanelManager.createOrShowPanelForPerfCommand(context);
-   // const sidebar_test = new sidebar.TreeViewProvider();
-   // vscode.window.registerTreeDataProvider("perfadvisorTools", sidebar_test);
-   // vscode.commands.executeCommand('setContext', 'ipconfig', true);
+    // 响应VSCode配置修改
+    Utils.addConfigListening();
+    // 清除Vscode缓存信息
+    Utils.initVscodeCache(context, true);
+    // 响应perfadvisor左侧菜单树所有按钮的命令来打开不同的webview
+    // console.log('here')
+    ToolPanelManager.createOrShowPanelForPerfCommand(context);
+
+    // const sidebar_test = new sidebar.TreeViewProvider();
+    // vscode.window.registerTreeDataProvider("perfadvisorTools", sidebar_test);
+    // vscode.commands.executeCommand('setContext', 'ipconfig', true);
+    // const provider = new SideViewProvider(context.extensionUri);
+    // context.subscriptions.push(
+    //     vscode.window.registerWebviewViewProvider(SideViewProvider.viewType, provider));
 }
 
-export function deactivate() { }
+export function deactivate() {
+}
